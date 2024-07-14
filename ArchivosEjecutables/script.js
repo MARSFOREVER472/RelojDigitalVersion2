d39d829c920ea589dee4d31c3a8941b11c3a5137
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-07-13 23:49:32 -0400

    Recursos agregados!!!

--- ArchivosEjecutables/script.js
+++ ArchivosEjecutables/script.js
@@ -1,4 +1,18 @@
 function generateTime()
 {
-    // EN INSTANTES...
+    let tiempoActual = new Date();
+
+
+let horas = tiempoActual.getHours().toString().length < 2 ? "0" + tiempoActual.getHours() : tiempoActual.getHours();
+let minutos = tiempoActual.getMinutes().toString().length < 2 ? "0" + tiempoActual.getMinutes() : tiempoActual.getMinutes();
+let segundos = tiempoActual.getSeconds().toString().length < 2 ? "0" + tiempoActual.getSeconds() : tiempoActual.getSeconds();
+
+let tiempoPrincipal = `${horas}:${minutos}:${segundos}`;
+document.getElementById("time").innerHTML = tiempoPrincipal;
+
 }
+
+setInterval(() => {
+    generateTime();
+}, 1000);
+
